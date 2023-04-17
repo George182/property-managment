8120a8b255064e8cb565d152c0bafa57c6f367bc
George <george.agasark@gmail.com>
2023-04-16 22:54:16 -0400

    adding logs

--- src/main/java/com/mycompany/propertymanagement/exception/CustomExceptionHandler.java
+++ src/main/java/com/mycompany/propertymanagement/exception/CustomExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.mycompany.propertymanagement.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -10,12 +11,17 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @ControllerAdvice
 public class CustomExceptionHandler {
 
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<List<ErrorModel>> handleBusinessException(BusinessException bex){
-        System.out.println("Business Exception is thrown");
+
+        for(ErrorModel em: bex.getErrors()){
+            log.info("Business Exception is thrown:  {} - {}", em.getCode(), em.getMessage());
+        }
+
         return new ResponseEntity<List<ErrorModel>>(bex.getErrors(), HttpStatus.BAD_REQUEST);
     }
 
@@ -27,6 +33,8 @@ public class CustomExceptionHandler {
         List<FieldError> fieldErrorList = manv.getBindingResult().getFieldErrors();
 
         for(FieldError fe: fieldErrorList){
+
+            log.info("inside field validation: {} - {}", fe.getField(), fe.getDefaultMessage());
 
             errorModel = new ErrorModel();
             errorModel.setCode(fe.getField());
