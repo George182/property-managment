6f89dd46c7544a129b523f2ba3a4ddb0ca1dbe37
George182 <32074802+George182@users.noreply.github.com>
2023-05-01 19:34:34 -0400

    gradle swagger

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
 	java
-	id("org.springframework.boot") version "2.7.8"
-	id("io.spring.dependency-management") version "1.0.15.RELEASE"
+	id("org.springframework.boot") version "2.5.9"
+	id("io.spring.dependency-management") version "1.0.11.RELEASE"
 }
 
 group = "com.mycompany"
@@ -22,7 +22,13 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
-	runtimeOnly("com.mysql:mysql-connector-j")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+
+	implementation("mysql:mysql-connector-java:8.0.32")
+	implementation("io.springfox:springfox-boot-starter:3.0.0")
+	implementation("io.springfox:springfox-swagger2:3.0.0")
+	implementation("io.springfox:springfox-swagger-ui:3.0.0")
+
 	compileOnly("org.projectlombok:lombok")
 	runtimeOnly("com.h2database:h2")
 	annotationProcessor("org.projectlombok:lombok")
